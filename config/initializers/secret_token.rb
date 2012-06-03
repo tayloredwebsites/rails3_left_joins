e0c31364ff5fd99437e731cad8aec579eb19d778
Dave Taylor <tayloredwebsites@me.com>
2012-06-03 08:59:48 -0400

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeftJoinTest::Application.config.secret_token = '516cdee146fba7758a820436c906aad62ced9cbabdf11e17da8e3b655ca96210786ff1327e0662d91b7a6af9441df5cc936eace08956e94869144a6504aaf6ed'
