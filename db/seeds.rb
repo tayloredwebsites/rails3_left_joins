be3e13febdc9e28facc41033c2ae4b9abf633449
Dave Taylor <tayloredwebsites@me.com>
2013-01-22 16:41:36 -0500

    create seed data, for dealing with heroku migration problems!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Lookup.create(
+  :name => 'Lookup Item 1',
+  :value => '1'
+)
+Lookup.create(
+  :name => 'Lookup Item 2',
+  :value => '2'
+)
+Lookup.create(
+  :name => 'Lookup Item 3',
+  :value => '3'
+)
+
+ThroughReference.create(
+  :description => 'Main Item 1'
+)
+ThroughReference.create(
+  :description => 'Main Item 2'
+)
+
+ThroughTable.create(
+:through_reference_id => 1,
+:lookup_id => 1,
+:description => 'Main Item 1 has Lookup item 1'
+)
+ThroughTable.create(
+:through_reference_id => 1,
+:lookup_id => 3,
+:description => 'Main Item 1 has Lookup item 3'
+)
+ThroughTable.create(
+:through_reference_id => 2,
+:lookup_id => 1,
+:description => 'Main Item 2 has Lookup item 1'
+)
+ThroughTable.create(
+:through_reference_id => 2,
+:lookup_id => 2,
+:description => 'Main Item 2 has Lookup item 2'
+)
